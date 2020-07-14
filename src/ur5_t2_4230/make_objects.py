2e1bb67fc964fff71b3c8b882fb0e55b953ed8ed
hamishchristianclark <z5061655@ad.unsw.edu.au>
2020-07-13 18:01:37 -0700

    1

--- src/ur5_t2_4230/make_objects.py
+++ src/ur5_t2_4230/make_objects.py
@@ -7,90 +7,100 @@ from std_msgs.msg import String
 from gazebo_msgs.srv import SpawnModel, DeleteModel
 from geometry_msgs.msg import Pose, Point, Quaternion
 
-def test():
-    print("hi")
-
 class URDF_Object:
-    orientation = Quaternion(0, 0, 0, 0)
-    item_pose = Pose(Point(x=0, y=0, z=0), orientation)
-    spawn_model = rospy.ServiceProxy("gazebo/spawn_urdf_model", SpawnModel)
-    delete_model = rospy.ServiceProxy("gazebo/delete_model", DeleteModel)
+	orientation = Quaternion(0, 0, 0, 0)
+	item_pose = Pose(Point(x=0, y=0, z=0), orientation)
+	spawn_model = rospy.ServiceProxy("gazebo/spawn_urdf_model", SpawnModel)
+	delete_model = rospy.ServiceProxy("gazebo/delete_model", DeleteModel)
 
-    def __init__(self, node_name, object_name, point):
-        '''Args: str, Point'''
+	def __init__(self, node_name, object_name, point):
+		'''Args: str, Point'''
 
-        # first try to delete it (it might already exist)
-        self.delete_model(node_name)
+		# first try to delete it (it might already exist)
+		self.delete_model(node_name)
 
-        MODEL_PATH = rospy.get_param('MODEL_PATH') + 'urdf/'
-	print(MODEL_PATH)
+		MODEL_PATH = rospy.get_param('MODEL_PATH') + 'urdf/'
+		print(MODEL_PATH)
 
-        with open(MODEL_PATH + object_name + '.urdf') as file:
-            xml = file.read()
-            item_pose = Pose(point, self.orientation)
-            self.spawn_model(node_name, xml, '', item_pose, 'world')
+		with open(MODEL_PATH + object_name + '.urdf') as file:
+			xml = file.read()
+			item_pose = Pose(point, self.orientation)
+			self.spawn_model(node_name, xml, '', item_pose, 'world')
 
 class SDF_Object:
-    orientation = Quaternion(0, 0, 0, 0)
-    item_pose = Pose(Point(x=0, y=0, z=0), orientation)
-    spawn_model = rospy.ServiceProxy("gazebo/spawn_sdf_model", SpawnModel)
-    
-    delete_model = rospy.ServiceProxy("gazebo/delete_model", DeleteModel)
-
-    def __init__(self, node_name, object_name, pose):
-        '''Args: str, Point'''
-
-        # first try to delete it (it might already exist)
-        self.delete_model(node_name)
-        test()
+	orientation = Quaternion(0, 0, 0, 0)
+	item_pose = Pose(Point(x=0, y=0, z=0), orientation)
+	spawn_model = rospy.ServiceProxy("gazebo/spawn_sdf_model", SpawnModel)
 	
-        MODEL_PATH = rospy.get_param('MODEL_PATH') + 'sdf/'
-	print(MODEL_PATH)
-        with open(MODEL_PATH + object_name + '.sdf') as file:
-            xml = file.read()
-            # item_pose = Pose(point, self.orientation)
-            self.spawn_model(node_name, xml, '', pose, 'world')
+	delete_model = rospy.ServiceProxy("gazebo/delete_model", DeleteModel)
+
+	def __init__(self, node_name, object_name, pose):
+		#'''Args: str, Point'''
+
+		# first try to delete it (it might already exist)
+		self.delete_model(node_name)
+	
+		MODEL_PATH = rospy.get_param('MODEL_PATH') + 'sdf/'
+		print(MODEL_PATH)
+		with open(MODEL_PATH + object_name + '.sdf') as file:
+			xml = file.read()
+			# item_pose = Pose(point, self.orientation)
+			self.spawn_model(node_name, xml, '', pose, 'world')
 
 def main():
-    rospy.init_node("spawn_products_in_bins")
-    rospy.wait_for_service("gazebo/spawn_urdf_model")
-    rospy.wait_for_service("gazebo/spawn_sdf_model")
-    rospy.wait_for_service("gazebo/delete_model")
+	#rospy.init_node("spawn_products_in_bins")
+	rospy.wait_for_service("gazebo/spawn_urdf_model")
+	rospy.wait_for_service("gazebo/spawn_sdf_model")
+	rospy.wait_for_service("gazebo/delete_model")
 
-    #spawn the table in a predefined position
-    pose = Pose(Point(2, 2, 0), Quaternion(0, 1, 0, -1))
-    output_container = SDF_Object('output_container', 'container', pose)
+	#spawn the table in a predefined position
+	pose = Pose(Point(1.5, 1, 0.25), Quaternion(0, 1, 0, -1))
+	object_table = SDF_Object('object_table', 'table', pose)
 
-    #spawn ten objects in a random position within set bounds
-    object_list = ['red_box', 'green_box', 'blue_box', 'red_cylinder', 'green_cylinder', 'blue_cylinder']
-    object_counter = 10
-#delete_model = rospy.ServiceProxy("gazebo/delete_model", DeleteModel)
-    while object_counter != 0:
-    	selected_object = random.choice(object_list)
-    	#delete_model(str(object_counter))
-    	#x coordinate
-    	x = random.uniform(0.5,2.5)
+	#spawn ten objects in a random position within set bounds
+	object_list = ['red_box', 'green_box', 'blue_box', 'red_cylinder', 'green_cylinder', 'blue_cylinder']
+	object_counter = 10
+	x_coords = [0]
+	y_coords = [0]
 
-    	#y coordinate
-    	y = random.uniform(1,3)
-    	
-    	URDF_Object(str(object_counter), selected_object, Point(x, y, 0))
+	while object_counter != 0:
+		#makes a random choice from the list of available objects to spawn
+		selected_object = random.choice(object_list)
+
+		#x coordinate
+		x = random.uniform(0.4,1.6)
+		#y coordinate
+		y = random.uniform(0.4,1.6)
+		
+		#flag to work out when to terminate loop
+		found_coord = False
+		while found_coord == False:
+			#check if the coordinate has been done before
+			for (c) in zip(x_coords,y_coords):
+				#check current x coordinate against randomised
+				if ((x-0.025)< c[0] < (x+0.025)) & ((y-0.025) < c[1] < (y+0.025)):
+					print("matching coords")
+					x = random.uniform(0.4,1.6)
+					y = random.uniform(0.4,1.6)
+				else:
+					#print("unique coords")
+					found_coord = True	
+
+		x_coords.append(x)
+		y_coords.append(y)
+
+		#spawn object at random location
+		URDF_Object(str(object_counter), selected_object, Point(x, y, 1.5))
+
+		object_counter = object_counter - 1
 
 
-    	object_counter = object_counter - 1
-
-
-    #URDF_Object('red_box', 'red_box', Point(0, 0, 0.2))
-    # green_box = URDF_Object('green_box', 'green_box', Point(0, -0.2, 0.2))
-    # blue_box = URDF_Object('blue_box', 'blue_box', Point(0, 0.2, 0.2))
-    # red_cylinder = URDF_Object('red_cylinder', 'red_cylinder', Point(0, 0, 0.6))
-    # green_cylinder = URDF_Object('green_cylinder', 'green_cylinder', Point(0, -0.2, 0.6))
-    # blue_cylinder = URDF_Object('blue_cylinder', 'blue_cylinder', Point(0, 0.2, 0.6))
 
 
 if __name__ == '__main__':
-    try:
+	try:
 	#print(rospy.get_param('MODEL_PATH'))
-        main()
-    except rospy.ROSInterruptException:
-	pass
+		main()
+		
+	except rospy.ROSInterruptException:
+		pass
